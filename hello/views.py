7b6e6c5ad5da32db276af1383694527465795bdc
Cheyenne Mangum <cheyenne@hellolanding.com>
2022-05-12 00:00:07 -0500

    2 teapots + a migrated pg db

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,6 @@
+
+import os
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,10 +8,29 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    times = int(os.environ.get('TIMES', 3))
+    r = requests.get('https://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse(('<pre>' + r.text + '</pre>') * times )
 
+# combined them both above for TWO teapots yeeyee
+# def index(request):
+#     times = int(os.environ.get('TIMES', 3))
+#     return HttpResponse('Hello! ' * times)
 
+"""
+after generating first table via: heroku run python manage.py migrate
+Now you can access the https://beginner-python.herokuapp.com/db/ route again,
+and you’ll see a simple page update every time you access it:
+
+The code to access the database is straightforward,
+and makes use of a simple Django model called Greetings
+that you can find in hello/models.py.
+
+Whenever you visit the /db route of your app,
+the following method in the hello/views.py file is invoked
+which creates a new Greeting and then renders all the existing Greetings:
+"""
 def db(request):
 
     greeting = Greeting()
